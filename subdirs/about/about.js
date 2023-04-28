438c328fb794c720e4a2e18da8f8888d28f33910
normie <aragonisafferz@gmail.com>
2023-04-29 00:03:12 +0700

    remove tween

--- subdirs/about/about.js
+++ subdirs/about/about.js
@@ -1,8 +1,1 @@
-function home() {
-    TweenMax.to("body", 0.5, {
-        opacity: 0,
-        onComplete: function() {
-            window.location.href = "../../index.html";
-        }
-    });
-}
+

3eb570929be905069f559c05e9c2feb96f475c6b
normie <aragonisafferz@gmail.com>
2023-05-01 22:55:13 +0700

    version 1.1
    added post date
    new blog
    added old website
    and more tweaks

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function homeclickarticle() {
-    location.href = "subdirs/news/articles/hired.html"
+    location.href = "subdirs/news/articles/sad ghost/sad-ghost.html"
     sessionStorage.setItem("clickedhome", true)
 }
-
--- subdirs/news/news.js
+++ subdirs/news/news.js
@@ -1,4 +1,8 @@
 function newsclickarticle() {
-    location.href = "articles/hired.html"
+    location.href = "articles/sad ghost/sad-ghost.html"
     sessionStorage.setItem("clickedhome", false)
+}
+
+function news1() {
+    location.href = "articles/hired/hired.html"
 }

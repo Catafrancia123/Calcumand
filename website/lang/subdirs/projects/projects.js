9b65cbea79090459b941767e3509c979517e3152
normie <aragonisafferz@gmail.com>
2023-09-29 19:53:46 +0700

    version 1.1.2

---
+++ website/lang/subdirs/projects/projects.js
@@ -0,0 +1,11 @@
+function cmdlink() {
+  window.location.href = "https://progressbar95-community.itch.io/terminal95";
+}
+
+function sololearn() {
+  window.location.href = "https://sololearn.com"
+}
+
+function oldweb() {
+  window.location.href = "https://www.sololearn.com/compiler-playground/W07l1L38KZq9"
+}
